1a4eea9c609d3bde9214e5edd187de97e8f1d83c
Rafael Márquez <ing17romc@gmail.com>
2022-09-17 21:29:38 -0500

    added spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import 'edt-lib/index.scss'
+import React from 'react'
 import Head from 'next/head'
+import { UI } from 'edt-lib'
+import Router from 'next/router'
+
+import 'edt-lib/index.scss'
 
 function MyApp ({ Component, pageProps }) {
+	const [loading, setLoading] = React.useState(false)
+	React.useEffect(() => {
+		const start = () => {
+			// console.log('start')
+			setLoading(true)
+		}
+		const end = () => {
+			// console.log('findished')
+			setLoading(false)
+		}
+		Router.events.on('routeChangeStart', start)
+		Router.events.on('routeChangeComplete', end)
+		Router.events.on('routeChangeError', end)
+		return () => {
+			Router.events.off('routeChangeStart', start)
+			Router.events.off('routeChangeComplete', end)
+			Router.events.off('routeChangeError', end)
+		}
+	}, [])
 	return (
 		<>
 			<Head>
@@ -10,7 +33,7 @@ function MyApp ({ Component, pageProps }) {
 					rel="stylesheet"
 				/>
 			</Head>
-
+			<UI.Spinner show={loading}/>
 			<Component {...pageProps} />
 		</>
 	)
